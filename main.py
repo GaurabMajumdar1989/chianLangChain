49382653673ab648291cbcbb5408008ac31156a6
Gaurab <gaurav.majumdar10@gmail.com>
2026-08-08 19:13:34 +0530

    TavilySearch as a tool and traces validation in Langsmith

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
-from dotenv import load_dotenv  # pyright: ignore[reportMissingImports]
+from dotenv import load_dotenv
 load_dotenv()   
 
-from langchain.agents import create_agent  # pyright: ignore[reportMissingImports]
-from langchain.tools import tool  # pyright: ignore[reportMissingImports]
-from langchain_core.messages import HumanMessage  # pyright: ignore[reportMissingImports]
-from langchain_ollama import ChatOllama  # pyright: ignore[reportMissingImports]
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama  
+from langchain_tavily import TavilySearch
+# from tavily import TavilyClient
 
-@tool  # pyright: ignore[reportUndefinedVariable]
-def search(query: str) -> str:
-    """
-        Tool that searches over internet
-        Args:
-            query: the place to search for
-        Returns:
-            The waether of the place mentioned in query    
-    """
-    print(f"Searching for: {query}")
-    return "Kolkata is having heavy monsoon rain "
+# tavily = TavilyClient()
+
+
+# @tool
+# def search(query: str) -> str:
+#     """
+#         Tool that searches over internet
+#         Args:
+#             query: {weather of the place to search for}
+#         Returns:
+#             The weather of the place mentioned in query    
+#     """
+#     print(f"Searching for: {query}")
+#     return tavily.search(query=query)
 
 llm=ChatOllama(
     model="llama3.2:latest",
     temperature=0.2
 )
-tools=[search]
+tools=[TavilySearch()]
 
 agent101 = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello, Agent Rover from Main!")
-    agent_response = agent101.invoke({"messages":HumanMessage(content="What is the weather of Kolkata?")})
+    agent_response = agent101.invoke({"messages":HumanMessage(content="What is the weather of Kolkata today?")})
 
 
     
